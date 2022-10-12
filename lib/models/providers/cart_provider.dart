d99eee481a4c7181f620d8e9f781ee356fda70ae
⛅ Arnab Deka 🍭 <arnab.deka@gmail.com>
2022-10-12 14:14:13 -0700

    add basic cart models

---
+++ lib/models/providers/cart_provider.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_playground_shopping_app/models/cart_item.dart';
+
+class CartProvider with ChangeNotifier {
+  final Map<String, CartItem> _itemsByProductId = {};
+
+  Map<String, CartItem> get itemsByProductId => {..._itemsByProductId};
+
+  void addItem(String productId, double price, String title) {
+    if (_itemsByProductId.containsKey(productId)) {
+      _itemsByProductId.update(
+          productId,
+          (item) => CartItem(
+                id: item.id,
+                title: item.title,
+                quantity: item.quantity + 1,
+                price: item.price,
+              ));
+    } else {
+      _itemsByProductId.putIfAbsent(
+          productId,
+          () => CartItem(
+                id: DateTime.now().toString(),
+                title: title,
+                quantity: 1,
+                price: price,
+              ));
+    }
+  }
+}
