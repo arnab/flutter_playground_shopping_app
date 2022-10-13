cdf4cec137ce4b3f645f1d86c12eafd69ec90586
⛅ Arnab Deka 🍭 <arnab.deka@gmail.com>
2022-10-13 11:37:29 -0700

    product detail page

--- lib/widgets/screens/product_detail_screen.dart
+++ lib/widgets/screens/product_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:money2/money2.dart';
 import 'package:provider/provider.dart';
 
 import '../../models/providers/products_provider.dart';
@@ -13,13 +14,54 @@ class ProductDetailScreen extends StatelessWidget {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
     final product = Provider.of<ProductsProvider>(
       context,
-      // don't need to re-build widget if products collection is changed (e.g. new added, removed)
       listen: false,
     ).findById(productId);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Image.network(
+                  product.imageUrl,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              Money.fromNum(
+                product.price,
+                code: 'CAD',
+              ).format('S0.00'),
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
