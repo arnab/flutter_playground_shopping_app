4d35676075c517636bc880c1f7be567634fd68a6
⛅ Arnab Deka 🍭 <arnab.deka@gmail.com>
2022-10-17 10:14:33 -0700

    using TextFormField inside forms with various types of inputs

--- lib/widgets/screens/product_edit_screen.dart
+++ lib/widgets/screens/product_edit_screen.dart
@@ -10,11 +10,14 @@ class ProductEditScreen extends StatefulWidget {
 }
 
 class _ProductEditScreenState extends State<ProductEditScreen> {
+  final _priceNodeFocus = FocusNode();
+  final _descriptionNodeFocus = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Edit Product'),
+        title: const Text('Edit Product'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(10),
@@ -23,16 +26,37 @@ class _ProductEditScreenState extends State<ProductEditScreen> {
             child: Column(
               children: [
                 TextFormField(
-                  decoration: InputDecoration(
-                    label: Text('Title'),
-                  ),
+                  decoration: const InputDecoration(label: Text('Title')),
                   textInputAction: TextInputAction.next,
-                )
+                  onFieldSubmitted: (_) =>
+                      FocusScope.of(context).requestFocus(_priceNodeFocus),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(label: Text('Price')),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                  focusNode: _priceNodeFocus,
+                  onFieldSubmitted: (_) =>
+                      FocusScope.of(context).requestFocus(_descriptionNodeFocus),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(label: Text('Description')),
+                  maxLines: 3,
+                  keyboardType: TextInputType.multiline,
+                  focusNode: _descriptionNodeFocus,
+                ),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _priceNodeFocus.dispose();
+    _descriptionNodeFocus.dispose();
+    super.dispose();
   }
 }
